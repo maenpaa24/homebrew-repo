e9d9ce9a7d45adb2a147292cbfc4319d6d5f7e2c
Fabio <fabio.galan@uib.es>
2021-03-06 14:25:58 +0100

    created up-to-date individual casks for different macos version of the spanish electronic ID and removed old cask

--- Casks/dnie-spain-bigsur.rb
+++ Casks/dnie-spain-bigsur.rb
@@ -1,12 +1,12 @@
-cask 'dnie-spain' do
-  version '1.5.3'
-  sha256 '1668d60a1ec8393354f7a667db2d9f86b07e150060d36729a89189d79af1e9ff'
+cask 'dnie-spain-bigsur' do
+  version '1.6.3'
+  sha256 '9341cd73e7171542f8a5e1bf88c584a2065cfb07a25666230d7647253e5fdb4e'
 
   url "https://www.dnielectronico.es/descargas/software_MACOS/libpkcs11-dnie-#{version}.pkg", user_agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
   name 'DNIe'
   homepage 'https://www.dnielectronico.es/PortalDNIe/'
 
-  depends_on macos: '>= :catalina'
+  depends_on macos: '>= :big_sur'
 
   pkg "libpkcs11-dnie-#{version}.pkg"
 
