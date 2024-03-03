9ce096d26d21bf5f8ab0f17ec5ca8d4c3669ff83
maenpaa24 <maenpaa24@gmail.com>
2024-03-03 17:13:41 +0100

    Update dnie-spain.rb

--- Casks/dnie-spain.rb
+++ Casks/dnie-spain.rb
@@ -1,21 +1,21 @@
 cask 'dnie-spain' do
-  version '1.6.7'
+  version '1.6.8'
   name 'DNIe'
   homepage 'https://www.dnielectronico.es/PortalDNIe/'
 
   cpu = Hardware::CPU.arm? ? 'arm64' : 'x64'
 
-  url "https://www.dnielectronico.es/descargas/software_MACOS/libpkcs11-dnie-#{version}_#{cpu}.pkg", user_agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-
   if Hardware::CPU.arm?
-   sha256 'c5c06f63bb9295692585cd34a93825aa1b027a278f04dc01b690f9cfc6298bc6'
+   url "https://www.dnielectronico.es/descargas/software_MACOS/libpkcs11-dnie-#{version}_arm.pkg", user_agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+   sha256 '1eb6ef2117953ff787f8a6da6f26c2e9523ebcc2d91ad991c5350a8faab521ed'
    depends_on macos: '>= :big_sur'
+   pkg "libpkcs11-dnie-#{version}_arm.pkg"
  else
-   sha256 '9958018827196f7de45f0cbc88b1eb18edf5aace75827083df9a12bd61754fe6'
+   url "https://www.dnielectronico.es/descargas/software_MACOS/libpkcs11-dnie-#{version}.pkg", user_agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+   sha256 '7f4ff38a8747f0c161f9a684a566c1727368245484dfef150ac18c912eddef4b'
    depends_on macos: '>= :mojave'
+   pkg "libpkcs11-dnie-#{version}.pkg"
   end
-
-  pkg "libpkcs11-dnie-#{version}_#{cpu}.pkg"
 
   uninstall pkgutil: 'libpkcs11-dnie'
 end
